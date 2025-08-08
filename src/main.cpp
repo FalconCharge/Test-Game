efa7a5274837a4ba6d6252b7edbd1c6639c3fac6
FalconCharge <99128705+FalconCharge@users.noreply.github.com>
2025-08-07 21:07:46 -0300

    It's working Finally There quite a bit of build steps though and I don't wanna write them down tn

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,6 @@
-#include <GL/glew.h>
 #include <iostream>
 
 int main() {
-    std::cout << "GLEW version: " << glewGetString(GLEW_VERSION) << std::endl;
+    std::cout << "Hello from MyGame using WolfEngine!" << std::endl;
     return 0;
 }
-
-
-/*
-#include "gl/glew.h"
-#include "W_App.h"
-
-// Subclass wolf::App and implement update and render
-class MyGameApp : public wolf::App
-{
-public:
-    MyGameApp() : wolf::App("My Game Window") {}
-
-    void update(float dt) override
-    {
-        // Game update logic here
-        // For example: check inputs, update game state
-        if (isKeyDown(GLFW_KEY_ESCAPE))
-            glfwSetWindowShouldClose(getWindow(), GLFW_TRUE);
-    }
-
-    void render() override
-    {
-        // Rendering code here
-        glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        // Draw stuff ...
-    }
-};
-
-int main()
-{
-    MyGameApp app;
-    app.run();
-    return 0;
-}
-*/
